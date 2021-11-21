60ceaa1288a831fdd0b11900ef9a15fda7bdaa46
STRIX in Notnik_kg <mamasydykova_g@auca.kg>
2021-11-21 23:18:50 +0600

    on click

--- app/src/main/java/com/zara07/classwork/Animation.kt
+++ app/src/main/java/com/zara07/classwork/Animation.kt
@@ -3,9 +3,9 @@ package com.zara07.classwork
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class Animation : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.animation)
     }
 }
